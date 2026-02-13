4ee77d2a1d504622476df04469e1ca0e27a1bf00
kabirahmed77 <kabirahmed7703@gmail.com>
2026-02-13 15:50:54 +0530

    Add Day 5 PRactice Questions and fix file locking issues

---
+++ Day 5/Practice Questions/FilesQuestions/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace FilesQuestions
+{
+    internal class BinAll
+    {
+        static void Main()
+        {
+            FileStream fs = new FileStream(@"D:\files\all.txt",
+                FileMode.Create, FileAccess.Write);
+
+            BinaryWriter bw = new BinaryWriter(fs);
+
+            bw.Write(12);
+            bw.Write("Tripathi");
+            bw.Write(12.55);
+            bw.Write(true);
+
+            bw.Close();
+            fs.Close();
+
+            Console.WriteLine("Data Stored in File Successfully...");
+        }
+    }
+}
