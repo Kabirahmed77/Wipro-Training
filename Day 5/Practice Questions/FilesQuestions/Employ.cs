71c5826b340e231375f37d0dd54f936e81068c4a
kabirahmed77 <kabirahmed7703@gmail.com>
2026-02-13 16:20:06 +0530

    Successfully implemented Employ binary serialization for  Day 5

---
+++ Day 5/Practice Questions/FilesQuestions/Employ.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FilesQuestions
+{
+    [Serializable] 
+    internal class Employ
+    {
+       
+        public int Empno { get; set; }
+        public string Name { get; set; }
+        public string Dept { get; set; }
+        public string Desig { get; set; }
+        public double Basic { get; set; }
+
+        public Employ() { }
+    }
+}
---
+++ Day 5/Practice Questions/FilesQuestions/WriteEmploy.cs
@@ -0,0 +1,29 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilesQuestions
+{
+    internal class WriteEmploy
+    {
+        static void Main()
+        {
+            Employ employ = new Employ();
+            employ.Empno = 1;
+            employ.Name = "Tushar";
+            employ.Dept = "Java";
+            employ.Desig = "Programmer";
+            employ.Basic = 82833;
+            FileStream fs = new FileStream("employdata.txt", FileMode.Create, FileAccess.Write);
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            binaryFormatter.Serialize(fs, employ);
+            fs.Close();
+            Console.WriteLine("Employ Data Stored in File...");
+        }
+     
+    }
+}
