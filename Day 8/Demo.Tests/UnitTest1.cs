3c5d0c9ff0d68e035770f8f769abc130934e976c
kabirahmed77 <kabirahmed7703@gmail.com>
2026-02-17 16:13:13 +0530

    Sdd Day 8 Unit Testing Code

---
+++ Day 8/Demo.Tests/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿namespace Demo.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Demo demo = new Demo();
+            Assert.AreEqual("Welcome to Unit Testing...", demo.SayHello());
+        }
+        [Test]
+        public void Test2_Negative()
+        {
+            Demo demo = new Demo();
+            Assert.AreNotEqual("Hello World", demo.SayHello());
+        }
+    }
+}
---
+++ Day 8/Demo/Demo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    public class Demo
+    {
+        public string SayHello()
+        {
+            return "Welcome to Unit Testing...";
+        }
+    }
+}
